48793684b2f565d504d079a354aaa981436b85f8
Michael Leow <mleow@moneylion.com>
2019-04-21 10:57:45 +0800

    Summary: Add deps + update lock

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/Sinar/go-pardocs
 
 go 1.12
+
+require github.com/davecgh/go-spew v1.1.1
--- main.go
+++ main.go
@@ -3,10 +3,14 @@ package main
 import (
 	"flag"
 	"fmt"
+
+	"github.com/davecgh/go-spew/spew"
 )
 
 func main() {
 	fmt.Println("Welcome to go-pardocs!")
 
 	flag.Parse()
+
+	spew.Println("bob")
 }
